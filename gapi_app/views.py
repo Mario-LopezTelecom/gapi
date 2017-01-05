e68da4f56a565978cf93033bd84c2ce2176c39b6
Mario L <mario.lopezTelecom@gmail.com>
2017-01-05 16:55:41 +0100

    [BROKEN] Google Calendar API example.

--- gapi_app/views.py
+++ gapi_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from services import gcal_api_example
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the gapi index.")
+    return HttpResponse(gcal_api_example())
+    #return HttpResponse(gcal_api_example())
 
 # Create your views here.
