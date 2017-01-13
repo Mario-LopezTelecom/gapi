d5dbc9ee079b8553ca14004dea4d98f39ecb0b5f
Mario L <mario.lopezTelecom@gmail.com>
2017-01-13 19:31:08 +0100

    WiP: serializers.

--- gapi_app/models.py
+++ gapi_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from oauth2client.contrib.django_orm import CredentialsField, FlowField
 
+
 class FlowModel(models.Model):
     id = models.ForeignKey(User, primary_key=True)
     flow = FlowField()
@@ -12,3 +13,21 @@ class FlowModel(models.Model):
 class CredentialsModel(models.Model):
     id = models.ForeignKey(User, primary_key=True)
     credential = CredentialsField()
+
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class CalendarEvent(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    calendar_id = models.ForeignKey(Calendar)
+    description = models.CharField(max_length=100, blank=True, default='')
+    date_start = models.DateTimeField()
+
+
+
+
+
+
+
