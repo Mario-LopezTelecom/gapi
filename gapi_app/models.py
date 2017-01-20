348480644453b246eb91bf2f475f9bc023888f98
Mario L <mario.lopezTelecom@gmail.com>
2017-01-20 19:23:12 +0100

    First serializer for 'CalendarEvent'

--- gapi_app/models.py
+++ gapi_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from oauth2client.contrib.django_orm import CredentialsField, FlowField
 
 
+# OAuth related models
 class FlowModel(models.Model):
     id = models.ForeignKey(User, primary_key=True)
     flow = FlowField()
@@ -15,29 +16,13 @@ class CredentialsModel(models.Model):
     credential = CredentialsField()
 
 
+# gapi models
 class Calendar(models.Model):
     name = models.CharField(max_length=100)
 
 
 class CalendarEvent(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
     calendar_id = models.ForeignKey(Calendar)
     description = models.CharField(max_length=100, blank=True, default='')
     date_start = models.DateTimeField()
-
-
-class PieSector(models.Model):
-    calendar_id = models.ForeignKey(Calendar)
-    diagram_id = models.ForeignKey(PieDiagram)
-    size = models.FloatField()
-
-
-class PieDiagram(models.Model):
-    date_start = models.DateTimeField()
     date_end = models.DateTimeField()
-
-
-
-
-
-
--- gapi_app/serializers.py
+++ gapi_app/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
-from gapi_app.models import PieDiagram, PieSector
+from gapi_app.models import CalendarEvent
+
+
+class CalendarEventSerializer(serializers.ModelSerializer):
+    calendar_id = serializers.StringRelatedField
+    class Meta:
+        model = CalendarEvent
+        fields = ('calendar_id', 'description', 'date_start', 'date_end')
 
